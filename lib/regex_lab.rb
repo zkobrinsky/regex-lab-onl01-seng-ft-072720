aa81f7e1cebffa57a4603a62db132649fd28bec5
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-10 15:31:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiouAEIOU]\w+/) ? true : false
+  word.match(/\b^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  # text.scan(/^(un)$(ing)\w+/)
 end
 
 def words_five_letters_long(text)
