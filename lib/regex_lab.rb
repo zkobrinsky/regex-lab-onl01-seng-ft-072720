4ffd0ddb5c6f97ca3139d6567fdfb6f1a83adc03
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-09-18 10:33:27 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match(/^[A-Z].+[.!?:;,]$/) ? true : false
 
+    # alt option:
+    # text.scan(/^[A-Z]/) && text.scan(/[.!?:;,]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+    phone.match(/(\d{10}\b)|(\d{3}[- ]\d{3}[- ]\d{4}\b)|(\d{3}[- ]\d{4}\b)|(\d{7}\b)/) ? true : false
 end
